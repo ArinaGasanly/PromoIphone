84e7feeed6d233c3b210b78c2cf0f6eaeef0bdfc
Arina Gasanly <Arinakudinova111@yandex.ru>
2022-01-22 14:29:45 +0300

    добавление ворого урока

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,26 +1,34 @@
-const links = document.querySelectorAll('.header-menu__item a');
+const scrollFunc = () => {
+  const links = document.querySelectorAll('.header-menu__item a');
+  const linkCharacteristics = document.querySelector('.card-details__link-characteristics');
 
-seamless.polyfill();
 
-links.forEach((element) => {
-  element.addEventListener('click', (event) => {
-    event.preventDefault();
-    
-    const id = element.getAttribute('href').substring(1);
-    const section = document.getElementById(id);
+  const newArray = [...links, linkCharacteristics]
+  seamless.polyfill();
 
-    if (section) {
-     section.scrollIntoView({
-       behavior: 'smooth',
-       block: 'start'
-     });
-    } else {
-      seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
-        behavior: "smooth",
-        block: "center",
-        inline: "center",
-      });
-    }
+  newArray.forEach((element) => {
+    element.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      const id = element.getAttribute('href').substring(1);
+      const section = document.getElementById(id);
+
+      if (section) {
+        section.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      } else {
+        seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+          behavior: "smooth",
+          block: "center",
+          inline: "center",
+        });
+      }
+    });
   });
-});
 
+}
+
+
+scrollFunc()
